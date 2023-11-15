09bdba3ff2753b35fd71294f6ab488988b1462aa
James Redding <jdr161@case.edu>
2023-11-15 16:42:04 -0500

    add generator outlines

---
+++ event-producer/src/main/java/org/example/EventGenerator.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.UUID;
+
+public class EventGenerator {
+    public static UUID getId(){
+
+    }
+    public static String getDate(){
+
+    }
+    public static String getTime(){
+
+    }
+    public static String getTitle(){
+
+    }
+    public static String getDescription(){
+
+    }
+    public static String getHostEmail(){
+
+    }
+}
---
+++ event-producer/src/main/java/org/example/ParticipantGenerator.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.UUID;
+
+public class ParticipantGenerator {
+    public static UUID getId(){
+
+    }
+    public static String getName(){
+
+    }
+    public static String getEmail(){
+        
+    }
+}
