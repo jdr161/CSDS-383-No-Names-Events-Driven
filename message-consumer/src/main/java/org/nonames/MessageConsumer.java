59eb5190af299f823e543d54422d64562605ff10
James Redding <jdr161@case.edu>
2023-11-15 15:43:22 -0500

    message consumer complete(ly untested)

--- message-consumer/src/main/java/org/nonames/MessageConsumer.java
+++ message-consumer/src/main/java/org/nonames/MessageConsumer.java
@@ -3,16 +3,9 @@ package org.nonames;
 import com.rabbitmq.client.*;
 
 import javax.json.*;
-import java.io.ByteArrayInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.*;
+import java.net.*;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.UUID;
 
 
@@ -39,11 +32,10 @@ public class MessageConsumer {
         if(jsonObject.isNull("participantID")){
             createEvent(parseEventData(jsonObject));
         } else { // if is a register-participant message
-            for (Participant p: parseParticipantData(jsonObject)) {
-                createParticipant(p);
-                UUID eventId = UUID.fromString(jsonObject.getString("eventID"));
-                registerParticipant(eventId, p.getParticipantId());
-            }
+            Participant p = parseParticipantData(jsonObject);
+            createParticipant(p);
+            UUID eventId = UUID.fromString(jsonObject.getString("eventID"));
+            registerParticipant(eventId, p.getParticipantId());
         }
     }
 
@@ -54,7 +46,6 @@ public class MessageConsumer {
             return jsonObject;
         } catch (Exception e) {
             System.out.println("Could not parse JSON object: " + new String(body, StandardCharsets.UTF_8));
-            System.exit(-1);
         }
         return null;
     }
@@ -69,46 +60,129 @@ public class MessageConsumer {
         return new Event(uuid,date,time,title,description,hostEmail);
     }
 
-    private ArrayList<Participant> parseParticipantData(JsonObject jsonObject){
-        return null;
+
+    private Participant parseParticipantData(JsonObject jsonObject){
+        UUID uuid = UUID.fromString(jsonObject.getString("participantID"));
+        String name = jsonObject.getString("name");
+        String email = jsonObject.getString("email");
+        return new Participant(uuid, name, email);
     }
 
     private void createEvent(Event event) {
-//        try {
-//            String apiEndpoint = "http://localhost:3001/api/create-event";
-//
-//            URL url = new URI(apiEndpoint).toURL();
-//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//
-//            connection.setRequestMethod("POST");
-//            connection.setRequestProperty("Content-Type", "application/json");
-//            connection.setDoOutput(true);
-//            // Write the JSON data to the request body
-//            try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
-//                wr.writeBytes(eventData);
-//            }
-//
-//            int responseCode = connection.getResponseCode();
-//
-//            if (responseCode == HttpURLConnection.HTTP_OK) {
-//                System.out.println("Successfully created an event.");
-//            } else {
-//                System.out.println("Failed to create an event");
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (URISyntaxException e) {
-//
-//
-//        }
+        try {
+            String apiEndpoint = "http://localhost:3001/api/create-event";
+
+            URL url = new URI(apiEndpoint).toURL();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+
+            String eventData = createEventJson(event);
+            // Write the JSON data to the request body
+            try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
+                wr.writeBytes(eventData);
+            }
+
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                System.out.println("Successfully created an event.");
+            } else {
+                // TODO: mark message as dead-letter (idk what that means)
+                System.out.println("Failed to create an event");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String createEventJson(Event event){
+        JsonObject value = Json.createObjectBuilder()
+                .add("id", event.getId().toString())
+                .add("date", event.getDate())
+                .add("time", event.getTime())
+                .add("title", event.getTitle())
+                .add("description", event.getDescription())
+                .add("hostEmail", event.getHostEmail())
+                .build();
+        return value.toString();
     }
 
     private void createParticipant(Participant participant){
+        try {
+            String apiEndpoint = "http://localhost:3001/api/create-participant";
 
+            URL url = new URI(apiEndpoint).toURL();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+
+            String participantData = createParticipantJson(participant);
+            // Write the JSON data to the request body
+            try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
+                wr.writeBytes(participantData);
+            }
+
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                System.out.println("Successfully created a participant.");
+            } else {
+                // TODO: mark message as dead-letter (idk what that means)
+                System.out.println("Failed to create a participant");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String createParticipantJson(Participant participant){
+        JsonObject value = Json.createObjectBuilder()
+                .add("id", participant.getParticipantId().toString())
+                .add("name", participant.getParticipantName())
+                .add("email", participant.getParticipantEmail())
+                .build();
+        return value.toString();
     }
 
     private void registerParticipant(UUID eventID, UUID participantID){
+        try {
+            String endpoint = "http://localhost:3001/api/register-participant?participantId="
+                    + URLEncoder.encode(participantID.toString(), "UTF-8")
+                    + "&eventId=" + URLEncoder.encode(eventID.toString(), "UTF-8");
 
+            HttpURLConnection connection = connectToApi(endpoint, "PUT");
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode != HttpURLConnection.HTTP_OK) {
+                // TODO: mark message as dead-letter (idk what that means)
+            } else {
+                System.out.println("Successfully registered a participant.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Helper method to make HTTP connections
+    private static HttpURLConnection connectToApi(String endpoint, String requestMethod) throws IOException {
+        try {
+            URL url = new URI(endpoint).toURL();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod(requestMethod);
+            return connection;
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
 
